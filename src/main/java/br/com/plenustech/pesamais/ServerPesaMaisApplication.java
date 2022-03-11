a17e60d22787bf5ef7dfa47e934f4c6a779211d5
Mikeias Santos <mikeiassantos16@gmail.com>
2022-03-11 06:05:28 -0400

    Criação do Projeto

---
+++ src/main/java/br/com/plenustech/pesamais/ServerPesaMaisApplication.java
@@ -0,0 +1,13 @@
+package br.com.plenustech.pesamais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerPesaMaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerPesaMaisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/plenustech/pesamais/ServerPesaMaisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.plenustech.pesamais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerPesaMaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
